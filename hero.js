54c0e25562b659b4fd53c5d4298e5fbaf1d2fbb2
CHASESY <chase_sydow@baylor.edu>
2015-05-14 19:28:50 -0500

    Changed move

--- hero.js
+++ hero.js
@@ -181,7 +181,7 @@ var moves =
  };
 
 //  Set our heros strategy
-var  move =  moves.selfishDiamondMiner;
+var  move =  moves.carefulAssassin;
 
 // Export the move function here
 module.exports = move;
